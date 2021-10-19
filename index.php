62db586685b5265453817ccb6327c923dd13a570
ooma tell <otymitu@yahoo.com>
2021-10-19 18:15:08 -0400

    added modal

--- index.php
+++ index.php
@@ -1,177 +1,152 @@
 <!doctype html>
 <html lang="en">
 
-
-
 <head>
          <meta charset="utf-8">
          <meta http-equiv="x-ua-compatible" content="ie=edge">
          <title>SuperMovie_For_Iyad_App</title>
-        
-         
 
-<style>
-* {
-       box-sizing: border-box;
-}
+        <style>
+        * {
+        box-sizing: border-box;
+        }
 
-body {
-       margin: 0;
-       font-family: Arial;
-       font-size: 17px;
-}
+        body {
+        margin: 0;
+        font-family: Arial;
+        font-size: 17px;
+        }
 
-#myVideo{
-       object-fit: cover;
-       width: 100vw;
-       height: 100vh;
-       position: fixed;
-       top: 0;
-       left: 0;
-}
-
-.content {
+        #myVideo{
+        object-fit: cover;
+        width: 100vw;
+        height: 100vh;
         position: fixed;
         top: 0;
-        background: rgba(0, 0, 0, 0.5);
-        color: #f1f1f1;
-        width: 100%;
-        padding: 20px;
-    }
+        left: 0;
+        }
 
-h1{
-        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        color: #2ecc71;
-        letter-spacing: -.03em;
-        font-weight: 300;
-        font-size: 50px;
-}
+        .content {
+                position: fixed;
+                top: 0;
+                background: rgba(0, 0, 0, 0.5);
+                color: #f1f1f1;
+                width: 100%;
+                padding: 20px;
+        }
 
-input{
-        display: inline;
-        width: 230px;
-        height:30px;
-        background-color:#2ecc71;
-        color: rgb(9, 9, 9);
-        border-radius:35px;
-        border-color: rgb(0, 123, 255);
-}
+        h1{
+                font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                color: #2ecc71;
+                letter-spacing: -.03em;
+                font-weight: 300;
+                font-size: 50px;
+        }
 
-input:hover{
-        background:#ffff0b ;
-        color:black;
-}
+        input{
+                display: inline;
+                width: 230px;
+                height:30px;
+                background-color:#2ecc71;
+                color: rgb(9, 9, 9);
+                border-radius:35px;
+                border-color: rgb(0, 123, 255);
+        }
 
-#responseSearch{
-        color:#2ecc71;
+        input:hover{
+                background:#ffff0b ;
+                color:black;
+        }
 
-}
-#myBtn {
-        display: inline;
-        width: 130px;
-        height:30px;
-        background-color: #239b56;
-        color: whitesmoke;
-        border-radius:35px;
-        border-color: red;
-        border-bottom-left-radius: 7px;
-        border-top-right-radius: 7px;
-        margin: 15px;
-        cursor: pointer;
-}
-#myBtn:hover {
-        background:#ffff0b ;
-        color:black;
-}
+        #responseSearch{
+                color:#2ecc71;
 
-#btn {
-        display: inline;
-        width: 130px;
-        height:30px;
-        background-color: #239b56;
-        color: whitesmoke;
-        border-radius:35px;
-        border-color: red;
-        border-bottom-left-radius: 7px;
-        border-top-right-radius: 7px;
-        margin: 15px;
-        cursor: pointer;
-}
+        }
+        #myBtn {
+                display: inline;
+                width: 130px;
+                height:30px;
+                background-color: #239b56;
+                color: whitesmoke;
+                border-radius:35px;
+                border-color: red;
+                border-bottom-left-radius: 7px;
+                border-top-right-radius: 7px;
+                margin: 15px;
+                cursor: pointer;
+        }
+        #myBtn:hover {
+                background:#ffff0b ;
+                color:black;
+        }
 
-#btn:hover {
-        background:#ffff0b ;
-        color:black;
-}
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
+        #btn {
+                display: inline;
+                width: 130px;
+                height:30px;
+                background-color: #239b56;
+                color: whitesmoke;
+                border-radius:35px;
+                border-color: red;
+                border-bottom-left-radius: 7px;
+                border-top-right-radius: 7px;
+                margin: 15px;
+                cursor: pointer;
+        }
 
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
+        #btn:hover {
+                background:#ffff0b ;
+                color:black;
+        }
 
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
+        #modalBtn {
+                display: inline;
+                width: 130px;
+                height:30px;
+                background-color: #239b56;
+                color: whitesmoke;
+                border-radius:35px;
+                border-color: red;
+                border-bottom-left-radius: 7px;
+                border-top-right-radius: 7px;
+                margin: 15px;
+                cursor: pointer;
+        }
 
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
+        #modalBtn:hover {
+                background:#ffff0b ;
+                color:black;
+        }
+        </style>
 
 
 </head>
           <video autoplay muted loop id="myVideo">
           <source src="img/640.mp4" type="video/mp4">
           </video>
-<body>
+        <body>
+                <div class="content"> 
+                <h1>Super Movies API</h1>
+                <input type="text" id="inputMovies" placeholder="Search TvMaze..."/>
+                <button id="btn" name="GO" onclick="searchMovies()">Search_Movie</button>
 
+                        <button id="myBtn"onclick="myPlayPauseBtn()">Pause</button>
 
+                <br>
+                <br>
 
-<div class="content">
-
-  
-<h1>Super Movies API</h1>
-
-          <input type="text" id="inputMovies" placeholder="Search TvMaze..."/>
-
-          <button id="btn" name="GO" onclick="searchMovies()">Search_Movie</button>
-
-          <button id="myBtn"onclick="myPlayPauseBtn()">Pause</button>
-
-<br>
-<br>
-
-          <div id="responseSearch"> Search Result:</div>
+                <div id="responseSearch"> Search Result:</div>
+       
 
 </div>
 
 <script>
 
 function searchMovies() {
+
+          var modal = document.getElementById("myModal");
+
+                
           var inputMovies = document.getElementById("inputMovies").value;
           var endpoint = "https://api.tvmaze.com/search/shows";            
           var reqEndpointTvMaze = endpoint + "?q=" + inputMovies;      
@@ -180,24 +155,24 @@ function searchMovies() {
           var xhr = new XMLHttpRequest();//abreviation xhr: XMLHttpReques
               xhr.onreadystatechange = function() {
 
-     if (this.readyState == 4 && this.status == 200) {
-              output = JSON.parse(this.responseText);
+                if (this.readyState == 4 && this.status == 200) {
+                        output = JSON.parse(this.responseText);
 
-          var number_tv = output.length;
+                        var number_tv = output.length;
 
-     for (var i = 0; i < number_tv; i++) {
+                        for (var i = 0; i < number_tv; i++) {
 
-            console.log(output[i]);  
+                                console.log(output[i]);  
 
-          var url = output[i]['show']['url']; 
-          var name = output[i]['show']['name'];
-   
+                                var url = output[i]['show']['url']; 
+                                var name = output[i]['show']['name'];
+                        
 
-          var output_html  = "<div class = 'displayMovies'>"  + ">" + name + ">" + url +  "</div><br><br><br>";
+                                var output_html  = "<div class = 'displayMovies'>"  + ">" + name + ">" + url +  "</div><br><br><br>";
 
-          var currentHTML = document.getElementById("responseSearch").innerHTML;
-              output_html = currentHTML + output_html;
-              document.getElementById("responseSearch").innerHTML = output_html;
+                                var currentHTML = document.getElementById("responseSearch").innerHTML;
+                                output_html = currentHTML + output_html;
+                                document.getElementById("responseSearch").innerHTML = output_html;
 
 }
 }
@@ -225,5 +200,6 @@ function myPlayPauseBtn() {
 }
 
 </script>
+<button id="modalBtn">Open Modal</button>
 </body>
 </html>
